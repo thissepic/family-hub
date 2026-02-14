2275213b513b1c2d65dbc4c94196fb7dbc95dcd6
thissepic <alex.semm@icloud.com>
2026-02-14 15:34:11 +0100

    fix: use direct path to prisma CLI in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Prisma migrations..."
-npx prisma migrate deploy --schema=./packages/db/prisma/schema.prisma
+node ./node_modules/prisma/build/index.js migrate deploy --schema=./packages/db/prisma/schema.prisma
 echo "Migrations complete."
 
 echo "Starting application..."
